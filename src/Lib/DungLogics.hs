df2511a5d40e2b98084ba5588d666670642838c5
felixlinker <linkerfelix@gmail.com>
2019-03-01 17:49:02 +0100

    Remove sort dependency

--- src/Lib/DungLogics.hs
+++ src/Lib/DungLogics.hs
@@ -6,7 +6,7 @@ module Lib.DungLogics
 
 import qualified Language.Dung.AF as AF
 import qualified Data.Set as Set
-import qualified Data.Sort as Sort
+import qualified Data.List as List
 import Lib.Internal
 
 type Kernel a = AF.DungAF a -> AF.DungAF a
@@ -14,18 +14,13 @@ type Kernel a = AF.DungAF a -> AF.DungAF a
 krFreeWith :: (Ord a) => Kernel a -> AF.DungAF a -> Bool
 krFreeWith k (AF.AF args atts) =
     let (AF.AF _ kAtts) = k $ AF.AF args atts
-    in (Sort.sort atts) == kAtts
+    in (Set.fromList atts) == (Set.fromList kAtts)
 
 union :: (Ord a) => AF.DungAF a -> AF.DungAF a -> AF.DungAF a
 union (AF.AF [] _) x = x
 union x (AF.AF [] _) = x
 union (AF.AF args1 atts1) (AF.AF args2 atts2) =
-    let newArgs = listUnion args1 args2
-        newAtts = listUnion atts1 atts2
-    in AF.AF newArgs newAtts
-    where
-        listUnion :: (Ord a) => [a] -> [a] -> [a]
-        listUnion l1 l2 = Set.elems $ Set.union (Set.fromList l1) (Set.fromList l2)
+    AF.AF (List.union args1 args2) (List.union atts1 atts2)
 
 expandWith :: (Ord a) => Kernel a -> AF.DungAF a -> AF.DungAF a -> Maybe (AF.DungAF a)
 expandWith k (AF.AF [] _) x = Just $ k x
