62f62536b69fbebdeff2fea7220ae1c5c90b025a
felixlinker <linkerfelix@gmail.com>
2019-05-16 18:15:24 +0200

    Fix subAFs function

--- src/Language/Dung/Logics/Internal.hs
+++ src/Language/Dung/Logics/Internal.hs
@@ -20,17 +20,12 @@ subAFs :: (Ord a) => AF.DungAF a -> [AF.DungAF a]
 subAFs (AF.AF args atts) =
     let subArgs = Subsets.subsetsDesc args (-1) $ length args
         subAtts = Subsets.subsetsDesc atts (-1) $ length atts
-    in subAFs' (Set.fromList atts) subArgs subAtts
+    in [ AF.AF args' atts'  | args' <- subArgs
+                            , atts' <- subAtts
+                            , attsInArgs args' atts' ]
 
-subAFs' :: (Ord a) => Set.Set (a, a) -> [[a]] -> [[(a, a)]] -> [AF.DungAF a]
-subAFs' _ (_:[]) _ = [AF.AF [] []]
-subAFs' baseAtts (_:args:subArgs) [] =
-    let atts = filterAttsInArgs (Set.fromList args) baseAtts
-        subAtts = Subsets.subsetsDesc (Set.toList atts) (-1) (Set.size atts)
-    in subAFs' baseAtts (args:subArgs) subAtts
-subAFs' baseAtts (args:subArgs) (atts:subAtts) =
-    AF.AF args atts : subAFs' baseAtts (args:subArgs) subAtts
-
-filterAttsInArgs :: (Ord a) => Set.Set a -> Set.Set (a, a) -> Set.Set (a, a)
-filterAttsInArgs args = let mem = (`Set.member` args)
-    in Set.filter ((||) <$> mem . fst <*> mem . snd)
+attsInArgs :: (Ord a) => [a] -> [(a, a)] -> Bool
+attsInArgs args =
+    let argsSet = Set.fromList args
+        mem = (`Set.member` argsSet)
+    in foldl (\red (a1, a2) -> red && mem a1 && mem a2) True
